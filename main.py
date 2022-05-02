382e04aa3ea19db819fc99d0055459aaa7e9a5df
Ubuntu <ubuntu@ip-172-31-67-195.ec2.internal>
2022-05-02 22:54:43 +0000

    Stuff

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route("/")
 def hello():
@@ -7,5 +10,18 @@ def hello():
 
 @app.route("/signup", methods= ["POST"])
 def sign_up():
-    pass
+    json = request.json
+    print(json)
+    
 
+
+    return {
+        "ok": "true"
+    }
+
+
+if __name__ == "__main__":
+    cert_path = "/home/ubuntu/.monke/fullchain.pem"
+    privatekey_path = "/home/ubuntu/.monke/privkey.pem"
+
+    app.run(host="0.0.0.0", port=6969, ssl_context=(cert_path, privatekey_path))
