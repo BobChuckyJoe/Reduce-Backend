97355391cfda89b12ad01074dcd9d7b7e2f224e4
BobChuckyJoe <bobchuckyjoe@gmail.com>
2022-05-05 22:07:52 -0500

    meh

--- db.py
+++ db.py
@@ -4,35 +4,39 @@ import boto3
 import nacl.pwhash
 
 from data import LabeledImage, User
+
 # I will assume the credentials are set up correctly inside creds.json
 with open("creds.json") as f:
     creds = json.load(f)
 ddb = boto3.resource("dynamodb", region_name="us-east-1",
-                    aws_access_key_id=creds["aws_access_key_id"],
-                    aws_secret_access_key=creds["aws_secret_access_key"])
+                     aws_access_key_id=creds["aws_access_key_id"],
+                     aws_secret_access_key=creds["aws_secret_access_key"])
 
 users = ddb.Table("Reduce_users")
 images = ddb.Table("Reduce_images")
+
 
 def add_user(user: User):
     users.put_item(
         Item=user.to_dict()
     )
 
+
 def get_user(user_email: str) -> User:
-    res = users.get_item(Key= {"email": user_email})
+    res = users.get_item(Key={"email": user_email})
     # User does not exist
     if "Item" not in res:
         return None
 
     return User()
 
+
 def add_img_to_user(user: User, image: Image):
     # Local changes
     users.image_ids.append(image.image_id)
-    
+
     # Add stuff to db tables
-    
+
     # Upload image
     images.put_item(
         Item=image.to_dict()
@@ -40,7 +44,7 @@ def add_img_to_user(user: User, image: Image):
     # Append image id to user entry
     users.update_item(
         Key={
-            "email":user.email,
+            "email": user.email,
         },
         UpdateExpression="SET image_ids = list_append(image_ids, :img_ids)",
         ExpressionAttributeValues={
--- main.py
+++ main.py
@@ -4,27 +4,37 @@ import uuid
 
 from flask import Flask, request
 from flask_cors import CORS
-
 import nacl.pwhash
 from nacl.exceptions import InvalidkeyError
+import requests
 
 from data import User
 from db import get_user, add_user
+
 app = Flask(__name__)
 CORS(app)
 
 # Users will use session ids to keep their local state
-sess_ids = dict()
+user_to_sess_id = dict()
+sess_id_to_user = dict()
+
+
+def add_sess_id(user):
+    session_id = uuid.uuid4()
+    user_to_sess_id[user] = session_id
+    sess_id_to_user[session_id] = user
+
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
-@app.route("/signup", methods= ["POST"])
+
+@app.route("/signup", methods=["POST"])
 def sign_up():
     json = request.json
     pprint(json)
-    
+
     user = get_user(json["email"])
     if user is not None:
         # User exists!
@@ -56,6 +66,7 @@ def sign_up():
                 "session_id": session_id
             }
 
+
 @app.route("/login", methods=["POST"])
 def login():
     json = request.json
@@ -68,12 +79,67 @@ def login():
     else:
         # Verify password
         try:
-            nacl.pwhash.verify(user[""])
+            nacl.pwhash.verify(json["password"])
         except InvalidkeyError:
             return {
                 "ok": "false",
                 "error_message": "Username or password is incorrect"
             }
+        if user not in user_to_sess_id:
+            add_sess_id(user)
+
+        return {
+            "ok": "true",
+            "session_id": user_to_sess_id[user]
+        }
+
+
+@app.route("/upload", methods=["POST"])
+def post():
+    # Users will post to
+    json = request.json
+    session_id = json["session_id"]
+    if session_id not in sess_id_to_user:
+        return {
+            "ok": "false",
+            "error_message": "Bad session ID (are you logged in?)"
+        }
+    else:
+        img = json["img"]
+        # Offload the inference step (maestro is a potato)
+        res = requests.post(url="https://lovey.cs.utexas.edu:12345/detect",
+                            json={
+                                "passphrase": "ligma",
+                                "img_data": img
+                            })
+
+        res_json = res.json()
+        if res.status_code != 200:
+            print("Something went wrong with post request")
+            return {
+                "ok": "false",
+                "error_message": "Something went wrong on our end! Sorry..."
+            }
+
+        return {
+            "ok": "true",
+            "num_bottles": res_json["num_bottles"],
+            "labeled_image": res_json["labeled_image"]
+        }
+
+
+@app.route("/check_sess", methods=["POST"])
+def check_sess():
+    json = request.json
+    if json["session_id"] in sess_id_to_user:
+        return {
+            "ok": "true"
+        }
+    else:
+        return {
+            "ok": "false"
+        }
+
 
 if __name__ == "__main__":
     cert_path = "/home/ubuntu/.monke/fullchain.pem"
